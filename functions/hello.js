dece4ec2b6560af5127a1da05467e6bb12315a8c
Jsgtaj <jasaj316@gmail.com>
2020-10-13 16:48:10 -0400

    trying to fix json

--- functions/hello.js
+++ functions/hello.js
@@ -23,6 +23,6 @@ exports.handler = async event => {
   return {
     statusCode: 200,
     headers: { "Access-Control-Allow-Origin": "*" },
-    body: JSON.stringify(voteArr),
+    body: JSON.parse(voteArr);
   };
 };
